67f4ba0c2f82c48aea1f8ba82e55f56ddae248cf
KarimMagdy96 <karimmagdy096@gmail.com>
2024-09-08 19:06:47 +0300

    up

--- src/app/_Components/input/page.tsx
+++ src/app/_Components/input/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import React from "react";
 import { BsSearch } from "react-icons/bs";
-
-const Input = () => {
+interface InputProps {
+  handelSearch: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  setLocation: React.Dispatch<React.SetStateAction<string>>;
+}
+const Input = ({ handelSearch, setLocation }: InputProps) => {
   return (
     <>
       <form className=" flex items-center md:w-2/4 w-full order-2 md:order-1">
         <input
+          onKeyDown={handelSearch}
+          onChange={(e) => setLocation(e.target.value)}
           className=" w-full bg-transparent border-b-2 placeholder-white outline-none text-white "
           type="text"
           placeholder="Enter city name"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,52 @@
 "use client";
 
-import Input from "./input/page";
+import { useState } from "react";
+import Input from "./_Components/input/page";
 
 const Home = () => {
+  const [data, setData] = useState<any>({});
+  const [location, setLocation] = useState<any>("");
+  const [error, setError] = useState<any>({});
+  let apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  let url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}=${location}&days=7&aqi=yes&alerts=yes
+`;
+
+  const handelSearch = async (e: any) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      console.log(url);
+    }
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Error fetching data");
+      }
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+
+      // setLocation("");
+      setError("");
+    } catch (error) {
+      setError("city not found");
+      setData({});
+    }
+  };
+  console.log(location);
   return (
     <>
       <div className=" bg-cover bg-gradient-to-r from-blue-500 to-blue-300 h-screen  ">
         <div className=" bg-white/25 w-full h-full flex flex-col ">
           {/* input and logos */}
           <div className=" flex flex-col md:flex-row justify-between items-center p-12">
-            <Input />
+            <Input setLocation={setLocation} handelSearch={handelSearch} />
             <h1 className=" mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl">
               Weather App.
             </h1>
           </div>
+          {data?.current?.temp_f}
         </div>
       </div>
     </>
