ab4f66055a2ee6b1951ec6349f7421fc795515d6
KarimMagdy96 <karimmagdy096@gmail.com>
2024-09-09 01:46:49 +0300

    up

--- src/app/_Components/forcast/page.tsx
+++ src/app/_Components/forcast/page.tsx
@@ -1,30 +1,112 @@
 import React from "react";
 
-interface day {
-  date: string;
-  day: {
-    maxtemp_f: number;
-    mintemp_f: number;
-    condition: {
-      icon: string;
-      text: string;
+interface WeatherData {
+  data?: {
+    location?: {
+      name?: string;
+      region?: string;
+      country?: string;
+      lat?: number;
+      lon?: number;
+      tz_id?: string;
+      localtime_epoch?: number;
+      localtime?: string;
+    };
+    current?: {
+      last_updated_epoch?: number;
+      last_updated?: string;
+      temp_c?: number;
+      temp_f?: number;
+      is_day?: number;
+      condition?: {
+        text?: string;
+        icon?: string;
+        code?: number;
+      };
+      wind_mph?: number;
+      wind_kph?: number;
+      wind_degree?: number;
+      wind_dir?: string;
+      pressure_mb?: number;
+      pressure_in?: number;
+      precip_mm?: number;
+      precip_in?: number;
+      humidity?: number;
+      cloud?: number;
+      feelslike_c?: number;
+      feelslike_f?: number;
+      windchill_c?: number;
+      windchill_f?: number;
+      heatindex_c?: number;
+      heatindex_f?: number;
+      dewpoint_c?: number;
+      dewpoint_f?: number;
+      vis_km?: number;
+      vis_miles?: number;
+      uv?: number;
+      gust_mph?: number;
+      gust_kph?: number;
+      air_quality?: {
+        co?: number;
+        no2?: number;
+        o3?: number;
+        so2?: number;
+        pm2_5?: number;
+        pm10?: number;
+        "us-epa-index"?: number;
+        "gb-defra-index"?: number;
+      };
+    };
+    forecast?: {
+      forecastday?: {
+        date?: string;
+        date_epoch?: number;
+        day?: {
+          maxtemp_c?: number;
+          maxtemp_f?: number;
+          mintemp_c?: number;
+          mintemp_f?: number;
+          avgtemp_c?: number;
+          avgtemp_f?: number;
+          maxwind_mph?: number;
+          maxwind_kph?: number;
+          totalprecip_mm?: number;
+          totalprecip_in?: number;
+          totalsnow_cm?: number;
+          avgvis_km?: number;
+          avgvis_miles?: number;
+          avghumidity?: number;
+          daily_will_it_rain?: number;
+          daily_chance_of_rain?: number;
+          daily_will_it_snow?: number;
+          daily_chance_of_snow?: number;
+          condition?: {
+            text?: string;
+            icon?: string;
+            code?: number;
+          };
+          uv?: number;
+        };
+      }[];
     };
   };
 }
-const Forcast = ({ data }: any) => {
+const Forcast = ({ data }: WeatherData) => {
   return (
     <>
       <div className=" grid grid-cols-2 md:grid-cols-3  lg:grid-cols-7 gap-8 text-xs sm:text-sm lg:text-base w-full ">
-        {data?.forecast?.forecastday?.map((day: day, i: any) => {
+        {data?.forecast?.forecastday?.map((day, i: number) => {
           return (
             <div
               key={i}
               className=" bg-white/40 p-2 text-center rounded-lg flex flex-col items-center"
             >
               <div>
-                {new Date(day.date).toLocaleDateString("en-US", {
-                  weekday: "short",
-                })}
+                {day.date
+                  ? new Date(day?.date).toLocaleDateString("en-US", {
+                      weekday: "short",
+                    })
+                  : ""}
               </div>
               <img
                 src={day?.day?.condition?.icon}
--- src/app/_Components/weather.tsx/page.tsx
+++ src/app/_Components/weather.tsx/page.tsx
@@ -6,30 +6,109 @@ import { BsSunsetFill } from "react-icons/bs";
 import { SiAirbyte } from "react-icons/si";
 import { MdOutlineVisibility } from "react-icons/md";
 import { FaArrowDownUpAcrossLine } from "react-icons/fa6";
-
-interface WeatherDetailsProps {
-  data: {
+interface WeatherData {
+  data?: {
+    location?: {
+      name?: string;
+      region?: string;
+      country?: string;
+      lat?: number;
+      lon?: number;
+      tz_id?: string;
+      localtime_epoch?: number;
+      localtime?: string;
+    };
     current?: {
-      wind_mph: number;
-      humidity: number;
-      wind_dir: string;
-      pressure_mb: number;
-      feelslike_f: number;
-      vis_km: number;
+      last_updated_epoch?: number;
+      last_updated?: string;
+      temp_c?: number;
+      temp_f?: number;
+      is_day?: number;
+      condition?: {
+        text?: string;
+        icon?: string;
+        code?: number;
+      };
+      wind_mph?: number;
+      wind_kph?: number;
+      wind_degree?: number;
+      wind_dir?: string;
+      pressure_mb?: number;
+      pressure_in?: number;
+      precip_mm?: number;
+      precip_in?: number;
+      humidity?: number;
+      cloud?: number;
+      feelslike_c?: number;
+      feelslike_f?: number;
+      windchill_c?: number;
+      windchill_f?: number;
+      heatindex_c?: number;
+      heatindex_f?: number;
+      dewpoint_c?: number;
+      dewpoint_f?: number;
+      vis_km?: number;
+      vis_miles?: number;
+      uv?: number;
+      gust_mph?: number;
+      gust_kph?: number;
+      air_quality?: {
+        co?: number;
+        no2?: number;
+        o3?: number;
+        so2?: number;
+        pm2_5?: number;
+        pm10?: number;
+        "us-epa-index"?: number;
+        "gb-defra-index"?: number;
+      };
     };
     forecast?: {
-      forecastday: {
-        astro: {
-          sunrise: string;
-          sunset: string;
+      forecastday?: {
+        date?: string;
+        date_epoch?: number;
+        astro?: {
+          sunrise?: string;
+          sunset?: string;
+          moonrise?: string;
+          moonset?: string;
+          moon_phase?: string;
+          moon_illumination?: string;
+          is_moon_up?: number;
+          is_sun_up?: number;
+        };
+        day?: {
+          maxtemp_c?: number;
+          maxtemp_f?: number;
+          mintemp_c?: number;
+          mintemp_f?: number;
+          avgtemp_c?: number;
+          avgtemp_f?: number;
+          maxwind_mph?: number;
+          maxwind_kph?: number;
+          totalprecip_mm?: number;
+          totalprecip_in?: number;
+          totalsnow_cm?: number;
+          avgvis_km?: number;
+          avgvis_miles?: number;
+          avghumidity?: number;
+          daily_will_it_rain?: number;
+          daily_chance_of_rain?: number;
+          daily_will_it_snow?: number;
+          daily_chance_of_snow?: number;
+          condition?: {
+            text?: string;
+            icon?: string;
+            code?: number;
+          };
+          uv?: number;
         };
       }[];
     };
   };
 }
-
-const Weather = ({ data }: WeatherDetailsProps) => {
-  if (!data.current) {
+const Weather = ({ data }: WeatherData) => {
+  if (data && !data?.current) {
     return null;
   }
 
@@ -45,9 +124,9 @@ const Weather = ({ data }: WeatherDetailsProps) => {
             <h3>Wind Speed</h3>
             <h3
               className="text-white bg-black/25 rounded-xl mt-1"
-              aria-label={`Wind Speed: ${data.current.wind_mph} mph`}
+              aria-label={`Wind Speed: ${data && data?.current?.wind_mph} mph`}
             >
-              {data.current.wind_mph} mph
+              {data && data?.current?.wind_mph} mph
             </h3>
           </div>
           <div>
@@ -59,9 +138,9 @@ const Weather = ({ data }: WeatherDetailsProps) => {
             <h3>Humidity</h3>
             <h3
               className="text-white bg-black/25 rounded-xl mt-1"
-              aria-label={`Humidity: ${data.current.humidity}%`}
+              aria-label={`Humidity: ${data?.current?.humidity}%`}
             >
-              {data.current.humidity}%
+              {data?.current?.humidity}%
             </h3>
           </div>
           <div>
@@ -73,9 +152,9 @@ const Weather = ({ data }: WeatherDetailsProps) => {
             <h3>Wind Direction</h3>
             <h3
               className="text-white bg-black/25 rounded-xl mt-1"
-              aria-label={`Wind Direction: ${data.current.wind_dir}`}
+              aria-label={`Wind Direction: ${data && data?.current?.wind_dir}`}
             >
-              {data.current.wind_dir}
+              {data && data?.current?.wind_dir}
             </h3>
           </div>
           <div>
@@ -87,9 +166,13 @@ const Weather = ({ data }: WeatherDetailsProps) => {
             <h3>Sunrise</h3>
             <h3
               className="text-white bg-black/25 rounded-xl mt-1 px-2"
-              aria-label={`Sunrise: ${data.forecast?.forecastday[0]?.astro.sunrise}`}
+              aria-label={`Sunrise: ${
+                data?.forecast?.forecastday &&
+                data?.forecast?.forecastday[0]?.astro?.sunrise
+              }`}
             >
-              {data.forecast?.forecastday[0]?.astro.sunrise}
+              {data?.forecast?.forecastday &&
+                data?.forecast?.forecastday[0]?.astro?.sunrise}
             </h3>
           </div>
           <div>
@@ -101,9 +184,15 @@ const Weather = ({ data }: WeatherDetailsProps) => {
             <h3>Sunset</h3>
             <h3
               className="text-white bg-black/25 rounded-xl mt-1 px-2"
-              aria-label={`Sunset: ${data.forecast?.forecastday[0]?.astro.sunset}`}
+              aria-label={`Sunset: ${
+                data?.forecast?.forecastday != undefined
+                  ? data?.forecast?.forecastday[0]?.astro?.sunset
+                  : ""
+              }`}
             >
-              {data.forecast?.forecastday[0]?.astro.sunset}
+              {data?.forecast?.forecastday != undefined
+                ? data?.forecast?.forecastday[0]?.astro?.sunset
+                : ""}
             </h3>
           </div>
           <div>
@@ -115,9 +204,11 @@ const Weather = ({ data }: WeatherDetailsProps) => {
             <h3>Air Pressure</h3>
             <h3
               className="text-white bg-black/25 rounded-xl mt-1"
-              aria-label={`Air Pressure: ${data.current.pressure_mb} hPa`}
+              aria-label={`Air Pressure: ${
+                data && data?.current?.pressure_mb
+              } hPa`}
             >
-              {data.current.pressure_mb} hPa
+              {data && data?.current?.pressure_mb} hPa
             </h3>
           </div>
           <div>
@@ -129,9 +220,9 @@ const Weather = ({ data }: WeatherDetailsProps) => {
             <h3>Feels Like</h3>
             <h3
               className="text-white bg-black/25 rounded-xl mt-1"
-              aria-label={`Feels Like: ${data.current.feelslike_f}°`}
+              aria-label={`Feels Like: ${data && data?.current?.feelslike_f}°`}
             >
-              {data.current.feelslike_f}°
+              {data && data?.current?.feelslike_f}°
             </h3>
           </div>
           <div>
@@ -143,9 +234,9 @@ const Weather = ({ data }: WeatherDetailsProps) => {
             <h3>Visibility</h3>
             <h3
               className="text-white bg-black/25 rounded-xl mt-1"
-              aria-label={`Visibility: ${data.current.vis_km} km`}
+              aria-label={`Visibility: ${data && data?.current?.vis_km} km`}
             >
-              {data.current.vis_km} km
+              {data && data?.current?.vis_km} km
             </h3>
           </div>
           <div>
